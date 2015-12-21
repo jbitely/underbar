de0b23a49834c6e121df6ddb1030bb729a64170c
Justin Bitely <jbitely@gmail.com>
2015-12-21 17:00:42 -0500

    'extend' passes all tests

--- src/underbar.js
+++ src/underbar.js
@@ -222,6 +222,12 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    _.each(arguments, function(source){
+      for (var property in source){
+        obj[property] = source[property];
+      }
+    });
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
